338cbb3e61927d8bbbc1e02f0791eac37f674d36
Pedro Luiz Apolonio Camboim <pedrocamboim@klever.io>
2023-05-13 18:53:20 -0300

    get rooms from rooms-api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,30 @@
+import { useRooms } from "@/hooks/swr/useRooms.js";
 import { Card } from "../components/Card/index.jsx";
 import { SearchBar } from "../components/SearchBar/index.jsx";
-import { items } from "../constants/index.js";
+import { CardSkeleton } from "@/components/Card/CardSkeleton.jsx";
 
 export default function Home() {
+  const { rooms, isLoading } = useRooms();
+
   return (
     <>
       <main>
         <div className="container">
           <SearchBar />
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-            {items?.map((item) => (
-              <div key={item?.id} className="col">
-                <Card item={item} />
-              </div>
-            ))}
+            {isLoading ? (
+              rooms?.map((item) => (
+                <div key={item?.id} className="col">
+                  <Card item={item} />
+                </div>
+              ))
+            ) : (
+              <>
+                {Array.from(new Array(6)).map((_, index) => (
+                  <CardSkeleton key={index} />
+                ))}
+              </>
+            )}
           </div>
         </div>
       </main>
--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,3 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL });
+export const rooms = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_ROOMS_API_URL,
+});
