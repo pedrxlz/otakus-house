f021d9dee7ea254205be8be2adfc0490fc52051b
Pedro Luiz Apolonio Camboim <pedrocamboim@klever.io>
2023-05-13 23:06:32 -0300

    show user active/expired travels

---
+++ src/hooks/swr/useUserTravels.js
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+import { api } from "../../services/api/index.js";
+
+const fetcher = (url, authorization) =>
+  api
+    .get(url, { headers: { authorization: authorization } })
+    .then((res) => res.data);
+
+export const useUserTravels = ({ expired }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+  const { data, mutate, isLoading, error } = useSWR(
+    user._id
+      ? `api/v1/booking/get-bookings?userId=${user._id}&expired=${expired}`
+      : null,
+    (url) => fetcher(url, user.authToken)
+  );
+
+  return {
+    travels: data,
+    mutate,
+    isLoading,
+    error: error?.response?.data?.error,
+  };
+};
--- src/pages/travels/index.jsx
+++ src/pages/travels/index.jsx
@@ -2,9 +2,11 @@ import { activeTravels, expiredTravels } from "../../constants/index.js";
 import TravelLine from "../../components/TravelLine/index.jsx";
 import { useState } from "react";
 import styles from "./Travels.module.css";
+import { useUserTravels } from "@/hooks/swr/useUserTravels.js";
 
 export default function Travels() {
   const [tabIndex, setTabIndex] = useState(0);
+  const { travels, isLoading } = useUserTravels({ expired: !!tabIndex });
 
   return (
     <div className="container">
@@ -30,14 +32,9 @@ export default function Travels() {
               </button>
             </div>
             <div className="d-flex gap-2 flex-column my-4">
-              {tabIndex === 0 &&
-                activeTravels?.map((travel) => (
-                  <TravelLine key={travel?.id} travel={travel} />
-                ))}
-              {tabIndex === 1 &&
-                expiredTravels?.map((travel) => (
-                  <TravelLine key={travel?.id} travel={travel} />
-                ))}
+              {travels?.map((travel) => (
+                <TravelLine key={travel?.id} travel={travel} />
+              ))}
             </div>
           </div>
         </div>
